d04b588034423a50bef4f4eb500492c0669d9a97
Hohyun Kim <hohyunkim@jinair.com>
2025-02-20 16:18:57 +0900

    change data dir --> ./data/payment/extract/work

--- src/payment/util.rs
+++ src/payment/util.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 use crate::payment::common::Payment;
 
 pub fn save_csv_file(settleco: &str, sales_date: &str, payments: Vec<Payment>) -> anyhow::Result<()> {
-    let filename = format!("./data/_{}_SP_{}.csv", settleco, sales_date);
+    let filename = format!("./data/payment/extract/work/_{}_SP_{}.csv", settleco, sales_date);
     let path = Path::new(filename.as_str());
     let mut wtr = csv::Writer::from_path(path)?;
     for p in payments {
